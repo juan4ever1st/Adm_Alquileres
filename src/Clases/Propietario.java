0f3f4c7c7316cf67217023b8c2aff0da2d25aa1e
opera020 <opera020@WS51L020.ribeiront.net.ar>
2018-12-04 19:15:48 -0300

    Actualización Parcial

--- src/Clases/Propietario.java
+++ src/Clases/Propietario.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 
 public class Propietario extends Persona {
+   
     public ArrayList getPropietarios(){
         ArrayList<Propietario> propietario = new ArrayList<>();
        //Aquí escribes el código con el que quieras rellenar la ArrayList
--- src/Clases/contrato.java
+++ src/Clases/contrato.java
@@ -1,10 +1,49 @@
 
 package Clases;
 
-class contrato {
-    Propietario p1 = new Propietario();
-    Inquilino in1 = new Inquilino();
-    Propiedad pr1 = new Propiedad();
+class contrato { //atributos
+    private Inquilino inquilino;
+    private Fecha fecha;
+    private Propietario propietario;
+    private Propiedad propiedad;
+    //constructor
+    public contrato(Inquilino inquilino, Fecha fecha, Propietario propietario, Propiedad propiedad){
+        this.inquilino = inquilino;
+        this.fecha = fecha;
+        this.propietario = propietario;
+        this.propiedad = propiedad;
+    }
+    //metodo getter para inquilino
+    public Inquilino getInquilino(){
+        return inquilino;
+    }
+     public void setInquilino(Inquilino inquilino){
+         this.inquilino = inquilino;
+     }
+
+    public Fecha getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Fecha fecha) {
+        this.fecha = fecha;
+    }
+
+    public Propietario getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Propietario propietario) {
+        this.propietario = propietario;
+    }
+
+    public Propiedad getPropiedad() {
+        return propiedad;
+    }
+
+    public void setPropiedad(Propiedad propiedad) {
+        this.propiedad = propiedad;
+    }
     
     
 }
